fcfe9df2a9ab48f1775a3d000dc5779fb398ba2a
Hoang Vu <vviethoang@gmail.com>
2022-10-06 13:15:44 +0700

    feat: update dict util

--- src/utils/dict_ultility.py
+++ src/utils/dict_ultility.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def deep_get(dictionary, keys, default=None):
     from functools import reduce
     return reduce(
@@ -11,22 +14,31 @@ def clean_dict(obj):
     }
 
 
-def to_dict(obj, classkey=None):
+def to_dict(obj, get_enum_value=True):
     if isinstance(obj, dict):
         data = {}
         for (k, v) in obj.items():
-            data[k] = to_dict(v, classkey)
+            data[k] = to_dict(v, get_enum_value)
+
         return data
     elif hasattr(obj, "_ast"):
-        return to_dict(obj._ast())
+        return to_dict(obj._ast(), get_enum_value)
     elif hasattr(obj, "__iter__") and not isinstance(obj, str):
-        return [to_dict(v, classkey) for v in obj]
+        return [to_dict(v, get_enum_value) for v in obj]
+    elif isinstance(obj, Enum):
+        return obj.value if get_enum_value else obj
     elif hasattr(obj, "__dict__"):
-        data = dict([(key, to_dict(value, classkey))
-                     for key, value in obj.__dict__.items()
-                     if not callable(value) and not key.startswith('_')])
-        if classkey is not None and hasattr(obj, "__class__"):
-            data[classkey] = obj.__class__.__name__
+        data = {}
+        for key in dir(obj):
+            if key.startswith('_'):
+                continue
+
+            attr = getattr(obj, key)
+            if callable(attr):
+                continue
+
+            data[key] = to_dict(attr, get_enum_value)
+
         return data
     else:
         return obj
